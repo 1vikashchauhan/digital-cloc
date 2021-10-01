6acd8406199507d9c4f292c8f89f3d2f05068fab
vikash chauhan <vc90841@gmail.com>
2021-10-01 14:43:17 +0530

    first commit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function startTime() {
     
     hr = (hr == 0) ? 12 : hr;
     hr = (hr > 12) ? hr - 12 : hr;
-    //Add a zero in front of numbers<10
+    
     hr = checkTime(hr);
     min = checkTime(min);
     sec = checkTime(sec);
